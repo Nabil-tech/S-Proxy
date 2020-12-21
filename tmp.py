09471979d1026b392e7d10013a1cc0928a7152ba
Nabil-tech <71862316+Nabil-tech@users.noreply.github.com>
2020-12-21 20:23:37 +0700

    Add files via upload

---
+++ tmp.py
@@ -0,0 +1,36 @@
+#Script Pencari  Proxy Host
+#By NBLCODE
+#Nyoba Recode Ya?
+#Ga Bisa bikin script bilang boss
+#intap intip nanti Matanya benjol hehehe
+R = '\033[91m'
+G = '\033[92m'
+B = '\033[94m'
+Y = '\033[93;1m'
+W = '\033[0;1m'
+C= '\033[35;1m'
+M= '\033[31;1m'
+
+logo = f"""{W}
+{W}       <{M}•{W}<{M}•{W}<{M}•{W}<{M}•{W}<{M}•{W}<{M}•{W}<{M}•{W}<{M}•{W}<{M}•{W}<{M}•{W}<{M}•{W}<{M}•{W}<{M}•{W}<{M}•{W}<{M}•{W}<{M}•{W}<{M}•{W}<{M}•{W}<
+       [{M}•{W}] {C}╭━╮╱╭┳━━╮╭╮╱╱╭━━━┳━━━┳━━━┳━━━╮{W}[{M}•{W}]
+  {W}     [{M}•{W}] {C}┃┃╰╮┃┃╭╮┃┃┃╱╱┃╭━╮┃╭━╮┣╮╭╮┃╭━━╯{W}[{M}•{W}]
+  {W}     [{M}•{W}] {C}┃╭╮╰╯┃╰╯╰┫┃╱╱┃┃╱╰┫┃╱┃┃┃┃┃┃╰━━╮{W}[{M}•{W}]
+  {W}     [{M}•{W}] {C}┃┃╰╮┃┃╭━╮┃┃╱╭┫┃╱╭┫┃╱┃┃┃┃┃┃╭━━╯{W}[{M}•{W}]
+  {W}     [{M}•{W}] {C}┃┃╱┃┃┃╰━╯┃╰━╯┃╰━╯┃╰━╯┣╯╰╯┃╰━━╮{W}[{M}•{W}]
+  {W}     [{M}•{W}] {C}╰╯╱╰━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━╯{W}[{M}•{W}] 
+  {W}     >{M}•{W}>{M}•{W}>{M}•{W}>{M}•{W}>{M}•{W}>{M}•{W}>{M}•{W}>{M}•{W}>{M}•{W}>{M}•{W}>{M}•{W}>{M}•{W}>{M}•{W}>{M}•{W}>{M}•{W}>{M}•{W}>{M}•{W}>{M}•{W}>
+  {W}
+                 
+
+ {W}  [{C}By{W}]    {W} : {Y}NBLCODE{W}
+ {W}  [{C}Contact {W}] {W}: {Y}https://www.facebook.com/nabil.fahmi.9275
+"""
+menu = f'''
+      {W}[{C}1{W}] CARI PROXY DAN CHECK
+      {W}[{C}2{W}] CARI HOST/BUG
+      {W}[{C}3{W}] PROXY RANDOM
+      {W}[{C}4{W}] CHECK PROXY in list FILE
+      {W}[{C}5{W}] CHECK SATU PROXY
+      {W}[{C}0{W}] KELUAR
+        '''
